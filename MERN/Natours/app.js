395e59aca3001db2eab191db1b4cf99b51de1628
Anshul Mishra <anshulm.dev@gmail.com>
2022-01-12 05:38:12 +0000

    Basic GET Req

--- MERN/Natours/app.js
+++ MERN/Natours/app.js
@@ -1,17 +1,21 @@
 const express = require('express');
+const fs = require('fs');
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
+
+app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
-    'data': 'Sample data',
-    'id': 122
-  });
+    "status":"success",
+    "results": tours.length,
+    "data": {tours}
+  })
 })
 
 
 app.listen(port, () => {
-  console.log('Running on port - 2000');
+  console.log(`Running on port - ${port}`);
 });
 
