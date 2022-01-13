a52d9b3dbb2c0a06cbbb09905ab26fd4e54db14e
Anshul Mishra <anshulm.dev@gmail.com>
2022-01-13 05:49:55 +0000

    POST endpoint

--- MERN/Natours/app.js
+++ MERN/Natours/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const fs = require('fs');
 
 const app = express();
-const port = 3000;
+app.use(express.json());
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
 
@@ -14,7 +14,17 @@ app.get('/api/v1/tours', (req, res) => {
   })
 })
 
+app.post('/api/v1/tours', (req, res) => {
+  const newid = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({id: newid}, req.body)
+  tours.push(newTour);
+  fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+    res.send(201).json(
+      {"status":"success", data:{tour:newTour}})
+  })
+})
 
+const port = 3000;
 app.listen(port, () => {
   console.log(`Running on port - ${port}`);
 });
