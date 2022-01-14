581b252adae04fe2457b4df02056a7d6cb8788e6
Anshul Mishra <anshulm.dev@gmail.com>
2022-01-14 13:41:16 +0000

    Single GET call

--- MERN/Natours/app.js
+++ MERN/Natours/app.js
@@ -14,6 +14,17 @@ app.get('/api/v1/tours', (req, res) => {
   })
 })
 
+app.get('/api/v1/tours/:id', (req, res) => {
+  const tour = tours.find((e) => e.id === req.params.id * 1)
+  if(req.params.id * 1 < tours.length){
+  res.status(200).json({
+    "status":"success",
+    "data": tour
+  })} else{
+    res.status(404).json({"status":"failed"})
+  }
+})
+
 app.post('/api/v1/tours', (req, res) => {
   const newid = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({id: newid}, req.body)
