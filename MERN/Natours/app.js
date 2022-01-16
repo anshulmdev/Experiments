1c5742d0c4b5892f757bd3d9f6af55b23e9986d4
Anshul Mishra <anshulm.dev@gmail.com>
2022-01-16 17:08:23 +0000

    Environments

--- MERN/Natours/app.js
+++ MERN/Natours/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const morgan = require('morgan');
+const dotenv = require('dotenv');
+dotenv.config({'path':'./config.env'})
+
 
 const app = express();
 app.use(express.json());
 app.use(morgan('dev'))
-
-
+app.use(express.static(`${__dirname}/public`))
 
 const tourRouter = require('./routes/tourRouters');
 const userRouter = require('./routes/userRoutes')
