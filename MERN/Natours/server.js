ef0dde11a8eb3e98297dcd73e5b84733c2e91eb2
Anshul Mishra <anshulm.dev@gmail.com>
2022-01-17 16:52:22 +0000

    Moongoose, MongoDB

--- MERN/Natours/server.js
+++ MERN/Natours/server.js
@@ -1,7 +1,11 @@
+const mongoose = require('mongoose');
 const app = require('./app');
 const dotenv = require('dotenv');
 
 dotenv.config({'path':'./config.env'})
+const DB = process.env.DATABASE;
+
+mongoose.connect(DB).then(connection => { console.log(connection.connections)});
 
 
 const port = process.env.PORT || 5000;
