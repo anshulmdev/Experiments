5a292461e144159836518a4429c7ef0f774044ba
Anshul Mishra <anshulm.dev@gmail.com>
2022-01-19 05:45:48 +0000

    moongooseModel

--- MERN/Natours/server.js
+++ MERN/Natours/server.js
@@ -7,6 +7,23 @@ const DB = process.env.DATABASE;
 
 mongoose.connect(DB).then(connection => { console.log(connection.connections)});
 
+const tourSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Name is mandatory'],
+    unique: true
+  },
+  rating: {
+    type: Number,
+    default: 4.5
+  },
+  price:{
+    type: Number,
+    required: [true, 'Price is mandatory']
+  }
+})
+
+const Tour = moongoose.model('Tour', tourSchema);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
