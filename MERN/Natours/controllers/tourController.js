7ffe5be3115f8e13b36bcedec0186ed564ee52f0
Anshul Mishra <anshulm.dev@gmail.com>
2022-01-22 11:39:15 +0000

    Patch Method

--- MERN/Natours/controllers/tourController.js
+++ MERN/Natours/controllers/tourController.js
@@ -40,15 +40,22 @@ exports.createTour = async (req, res) => {
   
 exports.getTour = async (req, res) => {
   try{
-    const tours = await Tour.findById(req.params.id)
-    await res.status(201).json({"status": "success", data: tours})
+    const tours = await Tour.findById(req.params.id);
+    await res.status(201).json({"status": "success", data: tours});
   }catch(err){
     res.status(404).json({"status":"Failed", "messgae": err.message})
   }
 }
   
-exports.updateTour = (req, res) => {
-    res.status(200).json({"status":"Tour Patched"})
+exports.updateTour = async (req, res) => {
+  try{
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true
+    });
+    await res.status(201).json({"status": "success", data: tour})
+  } catch(err){
+    res.status(404).json({"status":"Failed", "messgae": err.message})
+  }
   }
   
 exports.deleteTour = (req, res) => {
