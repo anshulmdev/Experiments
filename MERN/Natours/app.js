ed22010a4fbe55a3cfbca1fb65783536fe782513
Anshul Mishra <anshulm.dev@gmail.com>
2022-01-29 20:42:41 +0000

    unhandled Routes

--- MERN/Natours/app.js
+++ MERN/Natours/app.js
@@ -14,4 +14,9 @@ const userRouter = require('./routes/userRoutes')
 app.use('/api/v1/tours', tourRouter)
 app.use('/api/v1/users', userRouter)
 
+app.all('*', (req, res, next) => {
+    res.status(404).json({"status": "Failed", "message": `Can't find ${req.originalUrl} on server!!`})
+    next();
+})
+
 module.exports = app;
