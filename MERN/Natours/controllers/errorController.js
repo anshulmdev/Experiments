a841c9bbe347bc8d91c8a03d485d1c40552fd8fb
Anshul Mishra <anshulm.dev@gmail.com>
2022-02-01 17:31:53 +0000

    devs and prod errors

--- MERN/Natours/controllers/errorController.js
+++ MERN/Natours/controllers/errorController.js
@@ -1,5 +1,19 @@
+const sendErrorDev = (err, res) => {
+    return res.status(err.statusCode).json({"status": err.status, "message": err.message, "error": err.error, "stack": err.stack})
+}
+
+const sendErrorProd = (err, res) => {
+    if (err.isOperational) {
+        return res.status(err.statusCode).json({"status": err.status, "message": err.message})
+    } else {
+        return res.status(500).json({"status": "error", "message": "Something went wrong"})
+    }
+}
+
 module.exports = (err, req, res, next) => {
     err.statusCode = err.statusCode || 500;
     err.status = err.status || 'Failed';
-    res.status(err.statusCode).json({"status": err.status, "message": err.message})
+
+    if (process.env.NODE_ENV === 'development') sendErrorProd(err, res);
+    else if (process.env.NODE_ENV === 'production') sendErrorDev(err, res);
 }
