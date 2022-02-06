b5ea3b1513bd86e700a55473e062111b84738c14
Anshul Mishra <anshulm.dev@gmail.com>
2022-02-06 13:58:00 +0000

    consoleFile

---
+++ Node/console.js
@@ -0,0 +1,6 @@
+// Console Usage
+// console. error() writes to stderr , whereas console. log() writes to stdout
+
+console.log('Nomral Console: ', 'Normal Console Log');
+console.error('Console Error: ', new Error('Error Points'));
+console.warn('Console Warn: ', 'Warning Message');
