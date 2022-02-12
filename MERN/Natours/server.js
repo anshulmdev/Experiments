08e8d85197783c9052bfffb1ab8ddb623cb31c14
Anshul Mishra <anshulm.dev@gmail.com>
2022-02-12 09:07:45 +0000

    Closing server on unhandled errors

--- MERN/Natours/server.js
+++ MERN/Natours/server.js
@@ -3,15 +3,17 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({'path':'./config.env'})
-const DB = process.env.DATABASE || 'Invalid Connection String';
+const DB = process.env.DATABASEff || 'Invalid Connection String';
 
-mongoose.connect(DB).catch((err) => {console.log('Error occured :', err.message)});
+mongoose.connect(DB);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Running on port - ${port}`);
 });
 
-process.on('unhandledRejection', err =>{
+process.on('unhandledRejection', err => {
   console.log(err.name, err.message);
+  console.log('Unhandled error: Shutting Down')
+  process.exit(1);
 })
