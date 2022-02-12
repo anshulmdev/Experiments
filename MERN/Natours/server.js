e3003b4e998cf78d1f46dc71198a3d7ca180b55b
Anshul Mishra <anshulm.dev@gmail.com>
2022-02-12 09:16:22 +0000

    uncaughtException

--- MERN/Natours/server.js
+++ MERN/Natours/server.js
@@ -1,21 +1,37 @@
-const app = require('./app');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
-dotenv.config({'path':'./config.env'})
-const DB = process.env.DATABASEff || 'Invalid Connection String';
+process.on('uncaughtException', err => {
+  console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
 
-mongoose.connect(DB);
+dotenv.config({ path: './config.env' });
+const app = require('./app');
 
-const port = process.env.PORT || 5000;
-app.listen(port, () => {
-  console.log(`Running on port - ${port}`);
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+  .then(() => console.log('DB connection successful!'));
+
+const port = process.env.PORT || 3000;
+const server = app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
 });
 
 process.on('unhandledRejection', err => {
+  console.log('UNHANDLED REJECTION! 💥 Shutting down...');
   console.log(err.name, err.message);
-  console.log('Unhandled error: Shutting Down')
   server.close(() => {
     process.exit(1);
   });
-})
+});
