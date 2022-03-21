513da26679d297c12ab6a01740cd03660f75213d
Anshul Mishra <anshulm.dev@gmail.com>
2022-03-21 15:27:05 +0000

    Adapter Pattern

---
+++ Design Patterns/Adapter/localStorage.js
@@ -0,0 +1,36 @@
+const { readFileSync, writeFile, existsSync, unlink} = require('fs')
+
+
+class localStorage {
+
+  constructor () {
+    if(existsSync('localStorage.json')){
+      console.log('Local Storage File exists')
+      var txt = readFileSync('localStorage.json');
+      this.items = JSON.parse(txt);
+    } else this.items = {}
+  }
+
+  get length() {
+    return Object.keys(this.items).length;
+  };
+  getItem(key) {
+    return this.items[key]
+  };
+  setItem(key, value) {
+    this.items[key] = value;
+    writeFile('localStorage.json', JSON.stringify(this.items), error => {
+      if (error) console.log('Error Occured in writing file', error.message)
+    })
+  };
+  clear() {
+    this.items = {};
+    unlink('localStorage.json', () => {
+      console.log('Local storage is cleared')
+    })
+  };
+  
+}
+
+
+module.exports = new localStorage();
