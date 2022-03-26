fa29f07956ff6b1f6f4210f263c142685d58ed3c
Anshul Mishra <anshulm.dev@gmail.com>
2022-03-26 12:53:28 +0000

    Handler

--- Design Patterns/Handler/index.js
+++ Design Patterns/Handler/index.js
@@ -1,6 +1,7 @@
 var Store = require('./Store');
 var inventory = require('./inventory');
 
+var skiShop = new Store('Steep and Deep', inventory.floor);
 
 var searchItem = 'ski hats';
 var results = skiShop.find(searchItem);
