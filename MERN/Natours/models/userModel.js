ed438c7e14489381355e79eefcdfbbc945d356f8
Anshul Mishra <anshulm.dev@gmail.com>
2022-03-27 17:33:51 +0000

    bcrypt pass comparison

--- MERN/Natours/models/userModel.js
+++ MERN/Natours/models/userModel.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-const validator = require('validator')
-const bcrypt = require('bcryptjs')
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -42,6 +42,11 @@ userSchema.pre('save', async function(next) {
     this.passwordConfirm = undefined;
     next();
 })
+
+userSchema.methods.correctPassword = async function( candidatePassword, userPassword ) {
+    return await bcrypt.compare(candidatePassword, userPassword);
+}
+
 
 const User = mongoose.model('User', userSchema);
 
