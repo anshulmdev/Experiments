3ef6ed91c1e3ad0126bf5849646b040a9dcf6b22
Anshul Mishra <anshulm.dev@gmail.com>
2022-03-27 17:41:06 +0000

    jwtToken

--- MERN/Natours/controllers/authController.js
+++ MERN/Natours/controllers/authController.js
@@ -31,12 +31,11 @@ exports.login = catchAsync( async (req, res, next) => {
     if (!email || !password) return next(new AppError(`Please provide email and password`, 400));
     const user = await User.findOne({ email }).select('+password');
 
-    if (!user || !await User.correctPassword(password, user.password)){
+    if (!user || !await user.correctPassword(password, user.password)){
          return next(new AppError(`Incorrect email or password`, 401));
     }
 
     const token = signToken(user._id);
-    if (email !== token) return next(new AppError(`User not authenticated`, 400));
     res.status(200).json({
         status: 'success',
         token
