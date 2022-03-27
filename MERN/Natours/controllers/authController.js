6b418964a0d8f3caa320d852c3d464315d6bc1cb
Anshul Mishra <anshulm.dev@gmail.com>
2022-03-27 17:15:27 +0000

    logging

--- MERN/Natours/controllers/authController.js
+++ MERN/Natours/controllers/authController.js
@@ -6,13 +6,21 @@ const catchAsync = require('../utils/catchAsync');
 exports.signup = catchAsync(async (req, res, next) => {
     const { name, email, password, passwordConfirm } = req.body;
     const newUser = await User.create({ name, email, password, passwordConfirm });
-    const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
+    const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRES_IN
+    });
 
     res.status(201).json({
         status: 'success',
+        token,
         data: {
             user: newUser
         }
     })
     next();
 });
+
+
+exports.login = (req, res, next) => {
+    const { email } = req.body;
+}
