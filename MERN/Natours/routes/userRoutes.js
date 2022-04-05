3fc8347f73568d621506b3bfc3ca7e4d3bc05dc1
Anshul Mishra <anshulm.dev@gmail.com>
2022-04-05 19:44:41 +0000

    userRoutes

--- MERN/Natours/routes/userRoutes.js
+++ MERN/Natours/routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { getAllUsers, createUser, getUser, updateUser, deleteUser } = require('../controllers/userController');
-const { signup, login } = require('../controllers/authController');
+const { signup, login, protect, restrictTo } = require('../controllers/authController');
 const router = express.Router();
 
 router.post('/signup', signup);
@@ -15,6 +15,6 @@ router
     .route('/:id')
     .get(getUser)
     .patch(updateUser)
-    .delete(deleteUser);
+    .delete(protect, restrictTo('admin'), deleteUser);
 
 module.exports = router;
