b16b1a6b3b634b1b5bc13c2bc80c9a69b69d408b
Anshul Mishra <anshulm.dev@gmail.com>
2022-04-05 16:57:50 +0000

    enums

---
+++ TypeScript/src/Enums.ts
@@ -0,0 +1,24 @@
+enum Type {
+    "active" = "Active",
+    "inactive" = "Inactive",
+    "nan" = "Not Assigned"
+}
+
+
+interface Contact {
+    id: Number;
+    name: String;
+    birthDate?: Date;
+    status: Type
+}
+
+
+let sample2: String = "Example";
+
+
+let contactExample2: Contact = {
+    id: 11,
+    name: "John",
+    birthDate: new Date('11/11/1997'),
+    status: Type.active
+}
