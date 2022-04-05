2eb4c7f3842c9b308e1897a44b189f734b4ce10c
Anshul Mishra <anshulm.dev@gmail.com>
2022-04-05 17:24:07 +0000

    Generics

--- TypeScript/src/Generics.ts
+++ TypeScript/src/Generics.ts
@@ -1,23 +1,23 @@
 // function foo<T>(x: T): T { return x; }
+
 // Example - 1
 const myFunction = <T>(source: T): T => {
     return {...source};
 }
 
-const a = {id: 55, Name: "John"};
+const a = {id: 55, name: "John"};
 const b = myFunction(a);
 
 
 //Example - 2
-
 interface Contact {
     id: Number;
     name: String;
 }
 
-const myFunction = <T>(source: T): T => {
+const myFunction2 = <T1, T2>(source: T1): T1 => {
     return {...source};
 }
 
-const a = {id: 55, Name: "John"};
-const b = myFunction(a);
+const a1 = {id: 55, name: "John"};
+const b1 = myFunction2<Contact, Contact>(a1);
