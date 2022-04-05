faa7c68d47b03584e5af54f393f799ff5abd5ddb
Anshul Mishra <anshulm.dev@gmail.com>
2022-04-05 17:09:36 +0000

    func

--- TypeScript/src/Functions.ts
+++ TypeScript/src/Functions.ts
@@ -1,8 +1,18 @@
 interface myObject {
     id: Number;
     name: String;
+    value: Number;
 }
 
-const cloneContact = (source: myObject): myObject => {
-    return Object.apply({}, source)
+interface Contact {
+    id: Number;
+    name: String;
 }
+
+const cloneContact = (source: Contact): myObject => {
+    let sourceCopy: myObject = Object.apply({}, source);
+    sourceCopy.value = 10;
+    return sourceCopy
+}
+
+console.log(cloneContact({id: 55, name: "John"}))
