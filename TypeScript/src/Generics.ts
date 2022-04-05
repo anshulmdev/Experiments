cb9481c5f349cf93ed0546f773cafd2aca2a48a4
Anshul Mishra <anshulm.dev@gmail.com>
2022-04-05 17:22:22 +0000

    Generics

---
+++ TypeScript/src/Generics.ts
@@ -0,0 +1,17 @@
+// function foo<T>(x: T): T { return x; }
+// Example - 1
+const myFunction = <T>(source: T): T => {
+    return {...source};
+}
+
+const a = {id: 55, Name: "John"};
+const b = myFunction(a);
+
+
+//Example - 2
+
+interface Contact {
+    id: Number;
+    name: String;
+}
+
