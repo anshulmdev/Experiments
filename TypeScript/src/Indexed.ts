5bd079cf5dcb8515a550b2e376b20817ac3a42be
Anshul Mishra <anshulm.dev@gmail.com>
2022-04-09 09:21:39 +0000

    Indexed

---
+++ TypeScript/src/Indexed.ts
@@ -0,0 +1,15 @@
+interface ContactEvents {
+    deleted: String;
+    statusChanged: Boolean;
+}
+
+const getValue = <T U extends keyof T>(objectExample: T, propertyName: U) => {
+    return objectExample[propertyName];
+}
+
+const handleEvent = <T extends keyof ContactEvents>(
+    eventName: T,
+    handler: (evt: ContactEvents[T]) => void 
+) => {
+
+}
