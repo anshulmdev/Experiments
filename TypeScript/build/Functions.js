47745e9759ea923e72078449cb00712f710990c3
Anshul Mishra <anshulm.dev@gmail.com>
2022-04-10 10:36:48 +0000

    Migration of Files

--- TypeScript/build/Functions.js
+++
@@ -1,7 +0,0 @@
-const cloneContact = (source) => {
-    let sourceCopy = Object.assign({}, source);
-    console.log(sourceCopy);
-    sourceCopy["value"] = 10;
-    return sourceCopy;
-};
-console.log(cloneContact({ id: 55, name: "John" }));
--- TypeScript/build/Interface.js
+++
@@ -1,6 +0,0 @@
-let sample = "Example";
-let contactExample = {
-    id: 11,
-    name: "John",
-    birthDate: new Date('11/11/1997')
-};
