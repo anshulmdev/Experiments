09f7ddcb9f52b1da1b4b406dd7acf15945c10406
Anshul Mishra <anshulm.dev@gmail.com>
2022-04-29 15:50:52 +0000

    sinon

--- Node/Testing/Assert Usage/Basic/dependencyModule.js
+++ Node/Testing/Assert Usage/Basic/dependencyModule.js
@@ -2,12 +2,15 @@ function getSecretNumber() {
   return 44;
 }
 
-
 function getDate () {
   return String(new Date());
+}
+const sum = (a,b) => {
+  return a+b
 }
 
 module.exports = {
   getSecretNumber,
-  getDate
+  getDate,
+  sum
 };
