4d2307067185a8440f2427914cce1054d1cf51b1
Anshul Mishra <anshulm.dev@gmail.com>
2022-05-01 11:34:15 +0000

    sinon

--- MERN/Nadia/test/unit/lib/schema/reservation.js
+++ MERN/Nadia/test/unit/lib/schema/reservation.js
@@ -5,14 +5,6 @@ const Reservation = require('../../../../lib/schema/reservation');
 describe('Reservation Schema', function() {
 
   context('Validator', function() {
-    it('should pass a valid reservation with no optional fields', function(done) {
-      const reservation = new Reservation({
-        date: '2017/06/10',
-        time: '06:02 AM',
-        party: 4,
-        name: 'Family',
-        email: 'username@example.com'
-      });
 
       reservation.validator(function(error, value) {
         value.should.deep.equal(reservation);
