3668d4624a034a4bc58d66c218500ef8933c20d3
Anshul Mishra <anshulm.dev@gmail.com>
2022-05-07 10:21:22 +0000

    cypress

--- Testing/Cypress/cypress/integration/getUser.js
+++ Testing/Cypress/cypress/integration/getUser.js
@@ -6,7 +6,7 @@ describe("Get user api test", () => {
             method: "GET",
             url: "https://gorest.co.in/public/v2/users"
         }).then(res => {
-            cy.log(res.body)
+            cy.log(res.body[0].id)
             expect(res.status).to.eq(200)
             //expect(res.body.name).to.eq("John Doe")
         })
