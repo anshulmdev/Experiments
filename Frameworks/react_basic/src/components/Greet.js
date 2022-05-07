f449e007b89e58d52392794df8fe67d4399c5031
Anshul Mishra <anshulm.dev@gmail.com>
2022-05-07 19:33:47 +0000

    useState

--- Frameworks/react_basic/src/components/Greet.js
+++ Frameworks/react_basic/src/components/Greet.js
@@ -1,4 +1,4 @@
-
+import { useState } from 'react'
 import '../App.css';
 
 export const Greet = (props) => {
@@ -7,4 +7,14 @@ export const Greet = (props) => {
           {props.name}
         </div>
       );
+}
+
+export const reset = () => {
+  const [ message, setMessage ] = useState('Learning React 18')
+  return (
+    <div>
+      <h6>{ message }</h6> 
+      <button onClick={ () => setMessage("Heading updated via useState")}>Reset Heading</button>
+    </div>
+  )
 }
