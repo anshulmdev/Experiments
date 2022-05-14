4614d97f9075df5cb9ba39026bb57579375acd65
Anshul Mishra <anshulm.dev@gmail.com>
2022-05-14 20:45:28 +0000

    Challenge-1.ts

---
+++ TypeScript/Youtube/4-FunctionOverloading.ts
@@ -0,0 +1,11 @@
+/*
+Scopes
+ // Different variable structure object vs nums
+1. Prepare Interface
+2. Unknown Type
+3. Function Signatures
+4. Refer Video
+5. Runtime vs Compile Time
+6. As Keyword
+
+*/
