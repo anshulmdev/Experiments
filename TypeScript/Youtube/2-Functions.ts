8e79546c6e62aef54f3bfd337cba43a06daeaf8e
Anshul Mishra <anshulm.dev@gmail.com>
2022-05-15 08:09:36 +0000

    Functions

--- TypeScript/Youtube/2-Functions.ts
+++ TypeScript/Youtube/2-Functions.ts
@@ -1,14 +1,3 @@
-/*
-Scope
-1. Import and Export a Function [Default]
-2. Function variable types and return types
-3. Destructuring imports
-4. Union Types [Variable and Return]
-5. Promises
-6. Dynamic variables
-*/
-
-
 import { addition, promiseExample } from './2-Functions-2';
 
 
