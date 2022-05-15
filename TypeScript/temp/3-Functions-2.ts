256c352850bce974e1f40d5bc6849d4c7dbd6729
Anshul Mishra <anshulm.dev@gmail.com>
2022-05-15 12:07:36 +0000

    3-Functions-2.ts

--- TypeScript/temp/3-Functions-2.ts
+++ TypeScript/temp/3-Functions-2.ts
@@ -6,3 +6,13 @@ Scopes
 4. Closures
 
 */
+
+type additionPattern = (num1: number, num2: number) => number;
+
+const addNums = (num1: number, num2: number): number => { return num1 + num2 }
+
+const callBack1 = (var1: string, var2: string, addNums: additionPattern) => {
+    return addNums(+var1, +var2)
+}
+
+console.log(callBack1('9','9', addNums))
