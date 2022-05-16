83ff22f27b8d0571dbe1ed2c2f7044d162f0db31
Anshul Mishra <anshulm.dev@gmail.com>
2022-05-16 18:18:30 +0000

    Function Overloading

--- TypeScript/temp/4-FunctionOverloading.ts
+++ TypeScript/temp/4-FunctionOverloading.ts
@@ -22,12 +22,25 @@ const parseCoordinateFromNumber = (x: number, y: number): Coordinate => {
     return {x, y}
 }
 
-const parseCoordinate = (var1: unknown, var2?: unknown): Coordinate => {
-    let coord = {x: 0, y: 0}
-    if (typeof var1 === 'object') coord =  {...(var1 as Coordinate)}
-    else coord = {x: (var1 as number), y: (var2 as number)}
-    return coord
+///////////////////////////////////////
+
+function parseCoordinate(str: string): Coordinate;
+function parseCoordinate(obj: Coordinate): Coordinate;
+function parseCoordinate(x: number, y: number): Coordinate;
+function parseCoordinate (var1: unknown, var2?: unknown): Coordinate {
+    if (typeof var1 === 'object') return {...(var1 as Coordinate)}
+    else if (typeof var1 === 'number') return {x: (var1 as number), y: (var2 as number)}
+    else {
+        (var1 as string).split(",").forEach((str) => {
+            const [key, value] = str.split(":");
+            let coord = {}
+            coord[key as "x" | "y"] = parseInt(value, 10);
+            return coord
+          });
+
+    }
 }
 
 console.log(parseCoordinate(10, 20))
 console.log(parseCoordinate({x: 10, y: 20}))
+console.log(parseCoordinate("x:12,y:22"));
