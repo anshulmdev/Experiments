5a03ef89383f8205d3178adac63d7f523a04f512
Anshul Mishra <anshulm.dev@gmail.com>
2022-05-17 19:36:15 +0000

    Abstract

---
+++ TypeScript/Design Patterns/Creational/Abstract/sample.ts
@@ -0,0 +1,22 @@
+// The abstract factory interface declares a set of methods that
+// return different abstract products. These products are called
+// a family and are related by a high-level theme or concept.
+// Products of one family are usually able to collaborate among
+// themselves. A family of products may have several variants,
+// but the products of one variant are incompatible with the
+// products of another variant.
+interface GUIFactory is
+    method createButton():Button
+    method createCheckbox():Checkbox
+
+
+// Concrete factories produce a family of products that belong
+// to a single variant. The factory guarantees that the
+// resulting products are compatible. Signatures of the concrete
+// factory's methods return an abstract product, while inside
+// the method a concrete product is instantiated.
+class WinFactory implements GUIFactory is
+    method createButton():Button is
+        return new WinButton()
+    method createCheckbox():Checkbox is
+        return new WinCheckbox()
