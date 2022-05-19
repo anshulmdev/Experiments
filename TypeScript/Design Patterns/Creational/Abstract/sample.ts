103238ec20024e5d7aa9abedaf81b2bc1f5120d5
Anshul Mishra <anshulm.dev@gmail.com>
2022-05-19 18:57:09 +0000

    abstract

--- TypeScript/Design Patterns/Creational/Abstract/sample.ts
+++ TypeScript/Design Patterns/Creational/Abstract/sample.ts
@@ -9,12 +9,6 @@ interface GUIFactory is
     method createButton():Button
     method createCheckbox():Checkbox
 
-
-// Concrete factories produce a family of products that belong
-// to a single variant. The factory guarantees that the
-// resulting products are compatible. Signatures of the concrete
-// factory's methods return an abstract product, while inside
-// the method a concrete product is instantiated.
 class WinFactory implements GUIFactory is
     method createButton():Button is
         return new WinButton()
