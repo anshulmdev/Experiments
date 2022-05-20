da3ab3199200783fe954edd608c47d25d405dba7
Anshul Mishra <anshulm.dev@gmail.com>
2022-05-20 20:02:41 +0000

    Optionals ts

--- TypeScript/temp/5-Optional.ts
+++ TypeScript/temp/5-Optional.ts
@@ -4,3 +4,19 @@ Scopes
 2. Optional callbacks
 
 */
+
+interface optionalDeepObject {
+    name: string
+    data?: {
+        id: number
+        pin?: number
+        state? : string
+    }
+}
+
+const data1: optionalDeepObject = {
+    name: "John Doe",
+    data: {
+        id: 0
+    }
+}
