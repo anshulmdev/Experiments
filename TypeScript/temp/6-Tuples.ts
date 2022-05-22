225d800c55534520c0e8c7ce507aad0a7c3ca9cd
Anshul Mishra <anshulm.dev@gmail.com>
2022-05-22 08:11:25 +0000

    TS Practise

--- TypeScript/temp/6-Tuples.ts
+++ TypeScript/temp/6-Tuples.ts
@@ -1,3 +1,4 @@
 // custom 3d coordinate
 // what are tuples
 // closure
+// GET callback idea
---
+++ TypeScript/temp/7-Generics.ts
@@ -0,0 +1,4 @@
+1. Get basic idea of generics
+2. Start with tuples example
+3. Unknown vs Generics
+4. Need to follow video
