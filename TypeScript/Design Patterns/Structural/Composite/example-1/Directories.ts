e52c9b0d2bbbb3f6d6afc96958431a31b49e31cf
Anshul Mishra <anshulm.dev@gmail.com>
2022-05-23 19:26:32 +0000

    Abstract Pattern

--- TypeScript/Design Patterns/Structural/Composite/example-1/Directories.ts
+++ TypeScript/Design Patterns/Structural/Composite/example-1/Directories.ts
@@ -4,21 +4,5 @@ type Files = {
     print: () => {}
 }
 
-class Directories {
-    protected name: string
-    protected collection: Files[]
-
-    constructor (name: string, collection = [] ) {
-        this.name = name;
-        this.collection = collection;
-    }
-    get Total():number {
-        return +this.collection.reduce((total, item) => total + item.Total, 0)
-    }
-    print() {
-        console.log(`${this.name.toUpperCase()}`)
-        this.collection.forEach((e) => e.print())
-    }
-}
 
 export default Directories
