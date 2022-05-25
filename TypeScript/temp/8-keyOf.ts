ecb6aeeade53b3a60da14e60b67d25740fc27b55
Anshul Mishra <anshulm.dev@gmail.com>
2022-05-25 20:10:29 +0000

    Keyofs

---
+++ TypeScript/temp/8-keyOf.ts
@@ -0,0 +1,6 @@
+/**********************************************
+ * 1. PASS GENERIC ARRAY OF OBJECTS WITH KEYS *
+ * 2. USE KEY OF TO MAP AND PRINT VALUE      *
+ * 3. Add (merge) multiple types
+ * 4. Read an article on KeyOf
+ **********************************************/
