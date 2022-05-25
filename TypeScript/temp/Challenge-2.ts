3e7f607df72dab0c27d6c2a6c28a49830cf358da
Anshul Mishra <anshulm.dev@gmail.com>
2022-05-25 19:45:55 +0000

    challenge-2

--- TypeScript/temp/Challenge-2.ts
+++ TypeScript/temp/Challenge-2.ts
@@ -1,1 +1,11 @@
-TS
+// ForEach, Filter, Map
+
+const sample: number[] = [1, 2, 3, 4, 5];
+
+const sum = <T>(items: T[], forEachFunc: (v: T) => void): void => {
+    items.reduce((acc, index) => {
+        forEachFunc(index)
+        return undefined
+    }, undefined)
+}
+console.log(sum(sample, (v) => console.log(`For Each: ${v}`)))
