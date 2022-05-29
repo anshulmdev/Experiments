b072e5d2959851e23d1214aed8735e9414425a17
Anshul Mishra <anshulm.dev@gmail.com>
2022-05-29 15:35:58 +0000

    React useState

--- Frameworks/React/JH_React/src/App.js
+++ Frameworks/React/JH_React/src/App.js
@@ -1,7 +1,14 @@
 
 import './App.css';
+import axios from 'axios';
+import React from "react";
 
 function App() {
+  const [pokemons, setpokemons] = React.useState(null);
+  axios.get('https://gist.githubusercontent.com/jherr/23ae3f96cf5ac341c98cd9aa164d2fe3/raw/f8d792f5b2cf97eaaf9f0c2119918f333e348823/pokemon.json').then((response) => {
+    setpokemons(response)
+  })
+  console.log(pokemons)
   return (
     <div>
     <h6 className="App-header">
