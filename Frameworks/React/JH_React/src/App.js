e8bc77201ec004a327b09816e6de3cd482188644
Anshul Mishra <anshulm.dev@gmail.com>
2022-05-29 15:44:06 +0000

    dynamic

--- Frameworks/React/JH_React/src/App.js
+++ Frameworks/React/JH_React/src/App.js
@@ -6,27 +6,29 @@ import React from "react";
 function App() {
   const [pokemons, setpokemons] = React.useState(null);
   axios.get('https://gist.githubusercontent.com/jherr/23ae3f96cf5ac341c98cd9aa164d2fe3/raw/f8d792f5b2cf97eaaf9f0c2119918f333e348823/pokemon.json').then((response) => {
-    setpokemons(response)
+    setpokemons(response.data)
   })
   console.log(pokemons)
   return (
     <div>
     <h6 className="App-header">
-      Sample Heading
+      Pkemons
     </h6>
 
-    <table style= {{ margin: "auto" }}>
+    <table style={{ width: "100%" }}>
       <thead>
         <tr>
         <th>Name</th>
-        <th>Age</th>
+        <th>Type</th>
         </tr>
       </thead>
       <tbody>
-      <tr>
-        <td>John</td>
-        <td>35</td>
-      </tr></tbody>
+        { pokemons.map((el) => (
+          <tr>
+            <td>el.name.english</td>
+          </tr>
+        )) }
+      </tbody>
     </table>
 
 
