d0318c0cebfe122a5366046d6370184b23f71153
Anshul Mishra <anshulm.dev@gmail.com>
2022-06-03 17:36:54 +0000

    Prototype

---
+++ Node/Basic/Protoype.js
@@ -0,0 +1,20 @@
+// Base prototype.
+abstract class Shape is
+    field X: int
+    field Y: int
+    field color: string
+
+    // A regular constructor.
+    constructor Shape() is
+        // ...
+
+    // The prototype constructor. A fresh object is initialized
+    // with values from the existing object.
+    constructor Shape(source: Shape) is
+        this()
+        this.X = source.X
+        this.Y = source.Y
+        this.color = source.color
+
+    // The clone operation returns one of the Shape subclasses.
+    abstract method clone():Shape
