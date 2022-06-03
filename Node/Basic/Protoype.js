3196ccfc6894ceb555d676c494fcc03c9faeaf72
Anshul Mishra <anshulm.dev@gmail.com>
2022-06-03 17:37:07 +0000

    Prototype

--- Node/Basic/Protoype.js
+++ Node/Basic/Protoype.js
@@ -1,4 +1,4 @@
-// Base prototype.
+Base prototype.
 abstract class Shape is
     field X: int
     field Y: int
@@ -18,3 +18,35 @@ abstract class Shape is
 
     // The clone operation returns one of the Shape subclasses.
     abstract method clone():Shape
+
+
+// Concrete prototype. The cloning method creates a new object
+// and passes it to the constructor. Until the constructor is
+// finished, it has a reference to a fresh clone. Therefore,
+// nobody has access to a partly-built clone. This keeps the
+// cloning result consistent.
+class Rectangle extends Shape is
+    field width: int
+    field height: int
+
+    constructor Rectangle(source: Rectangle) is
+        // A parent constructor call is needed to copy private
+        // fields defined in the parent class.
+        super(source)
+        this.width = source.width
+        this.height = source.height
+
+    method clone():Shape is
+        return new Rectangle(this)
+
+
+class Circle extends Shape is
+    field radius: int
+
+    constructor Circle(source: Circle) is
+        super(source)
+        this.radius = source.radius
+
+    method clone():Shape is
+        return new Circle(this)
+
