1011f7bd71d09fc494362b545fc1ee8cedd70272
Anshul Mishra <anshulm.dev@gmail.com>
2022-06-04 15:34:57 +0000

    dnsTest.js

---
+++ Node/Basic/dnsTest.js
@@ -0,0 +1,21 @@
+const dns = require('dns');
+
+dns.lookup('google.in', (err, address,  family) => {
+    console.log('Address: %j family: IPv%s', address, family);
+});
+
+// IPV4 Addresses
+dns.resolve4('google.in', (err, addresses) => {
+    if (err) throw err;
+  
+    console.log(`addresses: ${JSON.stringify(addresses)}`);
+  
+    addresses.forEach((a) => {
+      dns.reverse(a, (err, hostnames) => {
+        if (err) {
+          throw err;
+        }
+        console.log(`Reverse for ${a}: ${JSON.stringify(hostnames)}`);
+      });
+    });
+  });
