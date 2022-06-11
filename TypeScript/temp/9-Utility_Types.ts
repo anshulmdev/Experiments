e017cb09258e921d4076ea831857a46714d54637
Anshul Mishra <anshulm.dev@gmail.com>
2022-06-11 11:20:52 +0000

    Utility Types

--- TypeScript/temp/9-Utility_Types.ts
+++ TypeScript/temp/9-Utility_Types.ts
@@ -1,7 +1,19 @@
-/****
- * CMD K,I
- * Partial Types
- * Required Types
- * Pick Types
- * Omit Types
- ****/
+
+interface User {
+    name: string;
+    id: string;
+    email?: string;
+}
+
+type OptionalUser = Partial<User> 
+type RequiredUser = Required<User>
+type CustomUser = Pick<User, "email" | "id">
+type CustomOmitUser = Omit<User, "id">
+
+const user = (user:User, opt: OptionalUser, req: RequiredUser): User => {
+    return {
+        ...user,
+        ...opt,
+        ...req
+    }
+}
