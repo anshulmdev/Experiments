e1a23b8adaaa81558d0b750c8eae9901982f7385
Anshul Mishra <anshulm.dev@gmail.com>
2022-06-11 11:38:56 +0000

    Immutable

--- TypeScript/temp/10_Immutable.ts
+++ TypeScript/temp/10_Immutable.ts
@@ -13,3 +13,28 @@ const dog = (name: string, age: number): Dog => {
 
 const example = dog("John", 12)
 example.name = "newName"
+
+
+// Example - 2
+
+interface Cood {
+    x: number,
+    y: number;
+    z: number
+}
+
+const makeCoordinates = (x: number, y: number, z:number): readonly Cood => {
+    return {
+        x, y, z
+    }
+}
+const exmaple2 = makeCoordinates(9,9,9);
+exmaple2.x = 90
+
+// Example - 3
+
+const makeCoordinates2 = (x: number, y: number, z:number): readonly [number, number, number] => {
+    return [x, y, z]
+}
+const example3 = makeCoordinates2(9,9,9);
+example3.x = 90
